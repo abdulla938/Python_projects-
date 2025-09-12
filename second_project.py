1f48c7f1a3885c05cbdf04de12a92801c1df0560
abdulla938 <abdullahshaid563@gmail.com>
2025-09-12 13:53:41 -0700

    Added python program demonstrating different data types
    “This program creates several variables in Python of various types:
    
    String: vone
    
    Integer: vtwo
    
    Float: vthree
    
    List: vfour
    
    Set: vfive
    
    Tuple: vsix
    
    Boolean: vseven
    
    Dictionary: veight
    
    
    It prints each variable and its type using the type() function to help beginners understand how Python stores and identifies different kinds of data.”

---
+++ second_project.py
@@ -0,0 +1,21 @@
+vone = ("Abdullah")
+print(type(vone))
+print(vone)
+vtwo = (34)
+print(type(vtwo))
+vthree = (66.78)
+print(type(vthree))
+vfour = [4,5,6]
+print(type(vfour))
+vfive = {1,2,3}
+print(type(vfive))
+vsix = (3,4,6)
+print(type(vsix))
+vseven = True
+print(type(vseven))
+print(vseven)
+veight = {'a':10,'b':20,'c':30,}
+print(type(veight))
+vnine = (2+4)
+print(vnine)
+
